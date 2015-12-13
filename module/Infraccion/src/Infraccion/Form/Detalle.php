2164c0ddae96f1e9470cc4d7231e0380d3bdd890
DHPONCE <DHPONCE>
2015-12-13 05:14:20 -0500

    backup

---
+++ module/Infraccion/src/Infraccion/Form/Detalle.php
@@ -0,0 +1,61 @@
+<?php 	//Detalle.php
+
+namespace Infraccion\Form;
+
+use Zend\Form\Form;
+use Zend\Form\Element\Select;
+
+date_default_timezone_set('America/Guayaquil');
+
+class Detalle extends Form {
+	function __construct($name = null) {
+		
+		parent::__construct($name);
+		
+
+
+		$this->add(array(
+         	'type' => 'Zend\Form\Element\Select',
+         	'name' => 'inmovilizado',
+         	'options' => array(
+                'label' => 'Inmovilizado:',
+                'value_options' => array(
+                    'f' => 'No',
+                    't' => 'Si'
+   				)
+			),	
+			'attributes' => array (
+				'id' => 'pun_rec_habilitado',
+				'class' => 'form-control'
+			)
+     	));
+
+		$this->add(array(
+         	'type' => 'Zend\Form\Element\Select',
+         	'name' => 'tiempo_permanencia',
+         	'options' => array(
+                'label' => 'Tiempo Permanencia:',
+                'value_options' => array(
+                    '0' => 'No hubo permanencia',
+                    '1' => '1 hora',
+                    '2' => '2 horas'
+				)
+			),	
+			'attributes' => array (
+				'id' => 'pun_rec_habilitado',
+				'class' => 'form-control'
+			)
+     	));
+	
+
+		$this->add ( array (
+			'name' => 'buscar',
+			'attributes' => array (
+					'type' => 'submit',
+					'value' => 'Aprobar Infracción',
+					'class' => 'btn btn-primary',
+					'data-loading-text' => 'Loading...'
+			)
+		) );
+	}
+}
---
+++ module/Infraccion/src/Infraccion/Form/DetalleValidator.php
@@ -0,0 +1,19 @@
+<?php 	//DetalleValidator.php
+
+namespace Infraccion\Form;
+
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\Input;
+use Zend\Validator\StringLength;
+use Zend\I18n\Validator\Alnum;
+use Zend\Validator\Digits;
+use Zend\Validator\NotEmpty;
+use Zend\Validator\Regex;
+
+class DetalleValidator extends InputFilter {
+	function __construct() {
+			
+
+
+	}
+}
